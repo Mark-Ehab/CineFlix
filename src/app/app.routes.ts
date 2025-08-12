a638480fa154e580cff84015661d72415122fb68
Mark <markehabtawfiq@gmail.com>
2025-08-12 07:05:03 +0300

    First Release

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"home", pathMatch:"full"},
+    {path:"home", component:Home, title:"Home"},
+    {path:"trendify", loadComponent:()=>import("./components/trendify/trendify").then((component)=>component.Trendify), title:"Trendify"},
+    {path:"products", loadComponent:()=>import("./components/products/products").then((component)=>component.Products), title:"Products"},
+    {path:"**", loadComponent:()=>import("./components/pagenotfound/pagenotfound").then((component)=>component.Pagenotfound), title:"404 - Page Not Found !"}
+];
